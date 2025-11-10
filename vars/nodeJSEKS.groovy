9fc91fafe2b3c4923ed53e9cb3988f4806174091
kashi <kashireddynaani9967@gmail.com>
2025-11-10 18:55:06 +0530

    shared lib

--- vars/nodeJSEKS.groovy
+++ vars/nodeJSEKS.groovy
@@ -1,53 +1,177 @@
-def call(Map configMap){
-    // mapName.get("key-name")
+// def call(Map configMap){
+//     // mapName.get("key-name")
+//     def component = configMap.get("component")
+//     echo "component is : $component"
+//     pipeline {
+//         agent { node { label 'AGENT-1' } }
+//         environment{
+//             //here if you create any variable you will have global access, since it is environment no need of def
+//             packageVersion = ''
+//         }
+        
+//         stages {
+//             stage('Get version'){
+//                 steps{
+//                     script{
+//                         def packageJson = readJSON(file: 'package.json')
+//                         packageVersion = packageJson.version
+//                         echo "version: ${packageVersion}"
+//                     }
+//                 }
+//             }
+//             stage('Install depdencies') {
+//                 steps {
+//                     sh 'npm install'
+//                 }
+//             }
+//             stage('Unit test') {
+//                 steps {
+//                     echo "unit testing is done here"
+//                 }
+//             }
+//             //sonar-scanner command expect sonar-project.properties should be available
+//             stage('Sonar Scan') {
+//                 steps {
+//                     echo "Sonar scan done"
+//                 }
+//             }
+//             stage('Build') {
+//                 steps {
+//                     sh 'ls -ltr'
+//                     sh "zip -r ${component}.zip ./* --exclude=.git --exclude=.zip"
+//                 }
+//             }
+//             stage('SAST') {
+//                 steps {
+//                     echo "SAST Done"
+//                     echo "package version: $packageVersion"
+//                 }
+//             }
+//             //install pipeline utility steps plugin, if not installed
+//             stage('Publish Artifact') {
+//                 steps {
+//                     nexusArtifactUploader(
+//                         nexusVersion: 'nexus3',
+//                         protocol: 'http',
+//                         nexusUrl: 'http://54.209.239.157:8081/',
+//                         groupId: 'com.roboshop',
+//                         version: "$packageVersion",
+//                         repository: "${component}",
+//                         credentialsId: 'nexus-auth',
+//                         artifacts: [
+//                             [artifactId: "${component}",
+//                             classifier: '',
+//                             file: "${component}.zip",
+//                             type: 'zip']
+//                         ]
+//                     )
+//                 }
+//             }
+
+//             stage('Docker Build') {
+//                 steps {
+//                     script{
+//                         sh """
+//                             docker build -t chinna9967/${component}:${packageVersion} .
+//                         """
+//                     }
+//                 }
+//             }
+//             // just make sure the we should login inside agent
+//             stage('Docker Push') {
+//                 steps {
+//                     script{
+//                         sh """
+//                             docker push chinna9967/${component}:${packageVersion}
+//                         """
+//                     }
+//                 }
+//             }
+            
+//             stage('EKS Deploy') {
+//                 steps {
+//                     script{
+//                         sh """
+//                             cd helm
+//                             sed -i "s/IMAGE_VERSION/${packageVersion}/g" values.yaml
+//                             helm upgrade ${component} -n roboshop . --set image.version=${packageVersion}
+//                         """
+//                     }
+//                 }
+//             }
+//             //here I need to configure downstram job. I have to pass package version for deployment
+//             // This job will wait until downstrem job is over
+//             // stage('Deploy') {
+//             //     steps {
+//             //         script{
+//             //             echo "Deployment"
+//             //             def params = [
+//             //                 string(name: 'version', value: "$packageVersion")
+//             //             ]
+//             //             build job: "../${component}-deploy", wait: true, parameters: params
+//             //         }
+//             //     }
+//             // }
+//         }
+
+//         post{
+//             always{
+//                 echo 'cleaning up workspace'
+//                 //deleteDir()
+//             }
+//         }
+//     }
+// }
+
+def call(Map configMap) {
+
     def component = configMap.get("component")
     echo "component is : $component"
+
     pipeline {
         agent { node { label 'AGENT-1' } }
-        environment{
-            //here if you create any variable you will have global access, since it is environment no need of def
-            packageVersion = ''
+
+        environment {
+            packageVersion = ""
         }
-        
+
         stages {
-            stage('Get version'){
-                steps{
-                    script{
-                        def packageJson = readJSON(file: 'package.json')
-                        packageVersion = packageJson.version
+
+            stage('Get version') {
+                steps {
+                    script {
+                        def pkg = readJSON(file: 'package.json')
+                        packageVersion = pkg.version
                         echo "version: ${packageVersion}"
                     }
                 }
             }
-            stage('Install depdencies') {
-                steps {
-                    sh 'npm install'
-                }
+
+            stage('Install dependencies') {
+                steps { sh 'npm install' }
             }
+
             stage('Unit test') {
-                steps {
-                    echo "unit testing is done here"
-                }
+                steps { echo "unit testing is done here" }
             }
-            //sonar-scanner command expect sonar-project.properties should be available
+
             stage('Sonar Scan') {
-                steps {
-                    echo "Sonar scan done"
-                }
+                steps { echo "Sonar scan done" }
             }
+
             stage('Build') {
                 steps {
-                    sh 'ls -ltr'
                     sh "zip -r ${component}.zip ./* --exclude=.git --exclude=.zip"
                 }
             }
+
             stage('SAST') {
                 steps {
                     echo "SAST Done"
                     echo "package version: $packageVersion"
                 }
             }
-            //install pipeline utility steps plugin, if not installed
+
             stage('Publish Artifact') {
                 steps {
                     nexusArtifactUploader(
@@ -55,69 +179,45 @@ def call(Map configMap){
                         protocol: 'http',
                         nexusUrl: 'http://54.209.239.157:8081/',
                         groupId: 'com.roboshop',
-                        version: "$packageVersion",
-                        repository: "${component}",
+                        version: packageVersion,
+                        repository: component,
                         credentialsId: 'nexus-auth',
-                        artifacts: [
-                            [artifactId: "${component}",
+                        artifacts: [[
+                            artifactId: component,
                             classifier: '',
                             file: "${component}.zip",
-                            type: 'zip']
-                        ]
+                            type: 'zip'
+                        ]]
                     )
                 }
             }
 
             stage('Docker Build') {
                 steps {
-                    script{
-                        sh """
-                            docker build -t chinna9967/${component}:${packageVersion} .
-                        """
-                    }
+                    sh "docker build -t chinna9967/${component}:${packageVersion} ."
                 }
             }
-            // just make sure the we should login inside agent
+
             stage('Docker Push') {
                 steps {
-                    script{
-                        sh """
-                            docker push chinna9967/${component}:${packageVersion}
-                        """
-                    }
+                    sh "docker push chinna9967/${component}:${packageVersion}"
                 }
             }
-            
+
             stage('EKS Deploy') {
                 steps {
-                    script{
-                        sh """
-                            cd helm
-                            sed -i 's/IMAGE_VERSION/$packageVersion/g' values.yaml
-                            helm upgrade ${component} -n roboshop .
-                        """
-                    }
+                    sh """
+                        cd helm
+                        helm upgrade ${component} -n roboshop . --set image.version=${packageVersion}
+                    """
                 }
             }
-            //here I need to configure downstram job. I have to pass package version for deployment
-            // This job will wait until downstrem job is over
-            // stage('Deploy') {
-            //     steps {
-            //         script{
-            //             echo "Deployment"
-            //             def params = [
-            //                 string(name: 'version', value: "$packageVersion")
-            //             ]
-            //             build job: "../${component}-deploy", wait: true, parameters: params
-            //         }
-            //     }
-            // }
         }
 
-        post{
-            always{
+        post {
+            always {
                 echo 'cleaning up workspace'
-                //deleteDir()
+                // deleteDir()
             }
         }
     }
